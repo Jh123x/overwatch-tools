9e9ec307b86c817eb2fd0993f9436fee43e28770
Wen Junhua <wenjh1998@gmail.com>
2022-04-02 12:59:03 -0400

    feat: add more info for lookup

--- src/pages/PlayerLookup.js
+++ src/pages/PlayerLookup.js
@@ -5,7 +5,16 @@ function PlayerLookupPage() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/55/Overwatch_circle_logo.svg" className='App-logo' alt='logo' />
+        <img
+          src='https://upload.wikimedia.org/wikipedia/commons/5/55/Overwatch_circle_logo.svg'
+          className='App-logo'
+          alt='logo'
+        />
+        <div>
+          <h1>Player Lookup toolkit</h1>
+          <p>Note that the name is case sensitive</p>
+          <p>It is in the format name#tag</p>
+        </div>
         <PlayerLookupForm />
       </header>
     </div>
@@ -13,5 +22,3 @@ function PlayerLookupPage() {
 }
 
 export default PlayerLookupPage;
-
-
