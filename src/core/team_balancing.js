8aed29e039b196a31cf8a2f7e0217120b0c2621f
Wen Junhua <wenjh1998@gmail.com>
2022-04-04 17:27:54 -0400

    feat: change balancing algorithm

--- src/core/team_balancing.js
+++ src/core/team_balancing.js
@@ -1,3 +1,13 @@
+function rating_comparator_desc(a, b) {
+  var keyA = parseInt(a.overall),
+    keyB = parseInt(b.overall);
+  // Compare the 2 rating
+  if (keyA < keyB) return 1;
+  if (keyA > keyB) return -1;
+  return 0;
+}
+
+
 export function player_balance_teams(players) {
   /// Balancing the number of players in an in-house game
   /// Player contains {playerName, battleTag, overall}
@@ -11,20 +21,19 @@ export function player_balance_teams(players) {
 
   let team1 = [];
   let team2 = [];
-  players.sort(function (a, b) {
-    var keyA = parseInt(a.overall),
-      keyB = parseInt(b.overall);
-    // Compare the 2 rating
-    if (keyA < keyB) return 1;
-    if (keyA > keyB) return -1;
-    return 0;
-  });
+  let team1_rating = 0;
+  let team2_rating = 0;
+  players.sort(rating_comparator_desc);
   for (let i = 0; i < players.length; ++i) {
-    if (i % 2 === 0) {
+    if (team1_rating <= team2_rating && team1.length < 6) {
       team1.push(players[i]);
+      team1_rating += players[i].overall;
       continue
     }
-    team2.push(players[i]);
+    if (team2.length < 6) {
+      team2.push(players[i]);
+      team2_rating += players[i].overall;
+    }
   }
   return [team1, team2];
 }
