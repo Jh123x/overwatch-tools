ac5c516311a18cb3cceeb4349aa95720523c0b42
Wen Junhua <wenjh1998@gmail.com>
2022-04-07 15:15:51 -0400

    feat: update player lookup with overall ranking

--- src/components/PlayerLookupForm.js
+++ src/components/PlayerLookupForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Input } from '@mui/material';
+import { Button, Input, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import styles from './PlayerLookupForm.module.css';
 import { player_lookup } from '../core/api';
 
@@ -26,6 +26,54 @@ export class PlayerLookupForm extends React.Component {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  generateRankURL(rating) {
+    let curr = 0;
+    if (rating < 1500) {
+      curr = 'Bronze';
+    }
+    else if (rating < 2000) {
+      curr = 'Silver';
+    }
+    else if (rating < 2500) {
+      curr = 'Gold';
+    }
+    else if (rating < 3000) {
+      curr = 'Platinum';
+    }
+    else if (rating < 3500) {
+      curr = 'Diamond';
+    }
+    else if (rating < 4000) {
+      curr = 'Master';
+    }
+    else if (rating < 4500) {
+      curr = 'Grandmaster';
+    }
+    return `https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-${curr}Tier.png`
+  }
+
+
+  wrap_table(key, iconUrl, rating, rankUrl, role) {
+    const icon = iconUrl.length > 0 ? <img src={iconUrl} alt={role} className={styles.icons} width="30px" /> : '';
+    if (rankUrl.length === 0) {
+      rankUrl = this.generateRankURL(rating);
+    }
+    const rank = <img src={rankUrl} alt={role} className={styles.icons} width="30px" />;
+    return <TableRow
+      key={key}
+      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+      <TableCell className={styles.text_color}>
+        {icon}
+        {this.Capitalize(role)}{" "}
+      </TableCell>
+      <TableCell className={styles.text_color}>
+        {rank}
+        {rating}
+      </TableCell>
+    </TableRow>
+  }
+
   render_response(response) {
     if (response.error === 'Player not found!' || response.status === 404) {
       return 'Player is not found!';
@@ -34,25 +82,14 @@ export class PlayerLookupForm extends React.Component {
       return 'Player Account is private! The Profile must be public to use this feature.';
     }
 
-    let table = [];
+    let table = [this.wrap_table(4, '', response.rating, '', 'Overall')];
     for (let i = 0; i < response.ratings.length; ++i) {
       const role = response.ratings[i].role;
       const iconUrl = response.ratings[i].roleIcon;
       const rankUrl = response.ratings[i].rankIcon;
       const rating = response.ratings[i].level;
       table.push(
-        <tr key={i}>
-          <td>
-            <img src={iconUrl} alt={role + ' icon'} />
-            {this.Capitalize(role)}{' '}
-          </td>
-          <td>
-            <div>
-              <img src={rankUrl} alt={rating + ' rank'} />
-              {rating}
-            </div>
-          </td>
-        </tr>
+        this.wrap_table(i, iconUrl, rating, rankUrl, role)
       );
     }
     return (
@@ -60,15 +97,19 @@ export class PlayerLookupForm extends React.Component {
         <h1>{this.state.playerName}</h1>
         <p>Level: {response.level + response.prestige * 100}</p>
         <h2>Rank:</h2>
-        <table>
-          <tbody>
-            <tr key='header'>
-              <th>Role</th>
-              <th>Rating</th>
-            </tr>
-            {table}
-          </tbody>
-        </table>
+        <TableContainer>
+          <Table aria-label="results table">
+            <TableHead>
+              <TableRow>
+                <TableCell className={styles.text_color}>Role</TableCell>
+                <TableCell className={styles.text_color}>Rating</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {table}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
@@ -125,7 +166,7 @@ export class PlayerLookupForm extends React.Component {
             placeholder='Battle Tag'
             className={styles.text_color}
             onChange={this.handleChange}
-            style= {{
+            style={{
               color: '#fa9c1e'
             }}
           />
