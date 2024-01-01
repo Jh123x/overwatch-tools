222f7c489cd01ce185e25f6206589f698777dc27
Wen Junhua <wenjh1998@gmail.com>
2024-01-01 17:27:56 +0800

    chore: remove web vitals

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import { Navbar } from './components/Navbar';
-import './index.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { Navbar } from "./components/Navbar";
+import "./index.css";
 
 // Pages
-import PlayerLookupPage from './pages/PlayerLookup';
-import Homepage from './pages/Homepage';
-import { TeamBalancingPage } from './pages/TeamBalancing';
-import { RandomPlayerPage } from './pages/RandomPlayerPick';
-import { PlayerMatchmake } from './pages/MatchMake';
+import PlayerLookupPage from "./pages/PlayerLookup";
+import Homepage from "./pages/Homepage";
+import { TeamBalancingPage } from "./pages/TeamBalancing";
+import { RandomPlayerPage } from "./pages/RandomPlayerPick";
+import { PlayerMatchmake } from "./pages/MatchMake";
 
-
-import reportWebVitals from './reportWebVitals';
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/team-balancing' element={<TeamBalancingPage />} />
-      <Route path='/lookup' element={<PlayerLookupPage />} />
-      <Route path='/random-player-pick' element={<RandomPlayerPage />} />
-      <Route path='/player-matchmake' element={<PlayerMatchmake />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/team-balancing" element={<TeamBalancingPage />} />
+      <Route path="/lookup" element={<PlayerLookupPage />} />
+      <Route path="/random-player-pick" element={<RandomPlayerPage />} />
+      <Route path="/player-matchmake" element={<PlayerMatchmake />} />
     </Routes>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
